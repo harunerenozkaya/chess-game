6b639ed822073a182f3e66215b11485057f38921
Harun Eren Özkaya <harun.eren.ozkaya@ozguryazilim.com.tr>
2022-09-10 18:33:39 +0300

    print board refactor

--- src/main/java/com/harun/chessgame/objects/Piece.java
+++ src/main/java/com/harun/chessgame/objects/Piece.java
@@ -6,18 +6,45 @@ import com.harun.chessgame.enums.PieceType;
 public class Piece {
     private PieceColor color;
     private PieceType type;
+    private String icon;
 
     public Piece(PieceColor color, PieceType type) {
         this.color = color;
         this.type = type;
+
+        switch (type){
+            case Rook:
+                icon = "R";
+                break;
+            case Knight:
+                icon = "K";
+                break;
+            case Bishop:
+                icon = "B";
+                break;
+            case Queen:
+                icon = "Q";
+                break;
+            case King:
+                icon = "X";
+                break;
+            case Pawn:
+                icon = "P";
+                break;
+            case Empty:
+                icon = " ";
+                break;
+        }
     }
 
     public PieceColor getColor() {
         return color;
     }
-
     public PieceType getType() {
         return type;
+    }
+    public String getIcon() {
+        return icon;
     }
 
     public void setColor(PieceColor color){
@@ -26,5 +53,8 @@ public class Piece {
 
     public void setType(PieceType type){
         this.type = type;
+    }
+    public void setIcon(String icon) {
+        this.icon = icon;
     }
 }
